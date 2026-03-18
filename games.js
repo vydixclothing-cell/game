87b2641a4b2ab07f45a4bf8efed586acdf11cb52
vydixclothing-cell <vydixclothing@gmail.com>
2026-03-18 22:20:26 +0530

    Game added: My Cake Shop: Bake & Serve

--- games.js
+++ games.js
@@ -1,3 +1,10 @@
-const games = [];
+const games = [
+    {
+        "name": "My Cake Shop: Bake & Serve",
+        "category": "Adventure",
+        "image": "https://img.gamedistribution.com/bd478cd5791e4568a2352f5a03d35ed1-512x512.jpg",
+        "url": "https://html5.gamedistribution.com/bd478cd5791e4568a2352f5a03d35ed1/"
+    }
+];
 
 export default games;
