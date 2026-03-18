73b833dc8e4a4e35327298b5c5356c9211f6c618
vydixclothing-cell <vydixclothing@gmail.com>
2026-03-18 22:23:53 +0530

    Game added: Stack Tower Pro

--- games.js
+++ games.js
@@ -1,3 +1,10 @@
-const games = [];
+const games = [
+    {
+        "name": "Stack Tower Pro",
+        "category": "Arcade",
+        "image": "https://img.gamedistribution.com/732542dbfb7f4804b95a8ece1e56545a-512x384.jpg",
+        "url": "https://gamedistribution.com/games/stack-tower-pro/"
+    }
+];
 
 export default games;
