6e9896a6a6a7e8c1bf45f41f3a131b6920481cc2
vydixclothing-cell <vydixclothing@gmail.com>
2026-03-18 22:04:48 +0530

    Game removed: Turn on Bulb

--- games.js
+++ games.js
@@ -1,60 +1,54 @@
 const games = [
-{
-    name: "Turn on Bulb",
-    category: "Puzzle",
-    image: "https://img.gamemonetize.com/s48fqhgjkr3e0ty4ylhk98wlghp0jxr0/512x384.jpg",
-    url: "https://html5.gamemonetize.co/s48fqhgjkr3e0ty4ylhk98wlghp0jxr0/"
-},
-{
-    name: "Water Ball Sorting Puzzle",
-    category: "Puzzle",
-    image: "https://img.gamemonetize.com/pvktpsvwhq58bo3pe7uqgxalqnwu67pr/512x340.jpg",
-    url: "https://html5.gamemonetize.co/pvktpsvwhq58bo3pe7uqgxalqnwu67pr/"
-},
-{
-    name: "Short Ride",
-    category: "Adventure",
-    image: "https://img.gamemonetize.com/2p9kpyev7iisztb9q8cwfyu9h4f9hlv1/512x384.jpg",
-    url: "https://html5.gamemonetize.co/2p9kpyev7iisztb9q8cwfyu9h4f9hlv1/"
-},
-{
-    name: "IRON SMASH",
-    category: "Arcade",
-    image: "https://img.gamemonetize.com/wf42ep9hw8py43cv4h4vgorep74ylimk/512x384.jpg",
-    url: "https://html5.gamemonetize.co/wf42ep9hw8py43cv4h4vgorep74ylimk/"
-},
-{
-    name: "Jump Masters",
-    category: "Arcade",
-    image: "https://img.gamemonetize.com/oc96sbhgq8x99xtl3ghoqk3nf85i72bs/512x384.jpg",
-    url: "https://html5.gamemonetize.co/oc96sbhgq8x99xtl3ghoqk3nf85i72bs/",
-    isNew: true
-},
-{
-    name: "MOMENTUM",
-    category: "Racing",
-    image: "https://img.gamemonetize.com/71kap8r3c2kj9n5rl4xt89khiwf2kowt/512x384.jpg",
-    url: "https://html5.gamemonetize.co/71kap8r3c2kj9n5rl4xt89khiwf2kowt/",
-    isFeatured: true
-},
-{
-    name: "Mountain Run",
-    category: "Racing",
-    image: "https://img.gamemonetize.com/releabfjx2tfx6owemnc1nb9cshzkjwg/512x384.jpg",
-    url: "https://html5.gamemonetize.co/releabfjx2tfx6owemnc1nb9cshzkjwg/"
-},
-{
-    name: "Stack Tower Pro",
-    category: "Arcade",
-    image: "https://img.gamemonetize.com/4kgh5yxxwvzmaixwo6kfzp4ocbu4e99b/512x384.jpg",
-    url: "https://html5.gamemonetize.co/4kgh5yxxwvzmaixwo6kfzp4ocbu4e99b/"
-},
-{
-    name: "Candy Rain",
-    category: "Arcade",
-    image: "https://img.gamemonetize.com/yx65q8unmpfskfwrqx7m7blv4de2s8jh/512x384.jpg",
-    url: "https://html5.gamemonetize.co/yx65q8unmpfskfwrqx7m7blv4de2s8jh/",
-    isNew: true
+    {
+        "name": "Water Ball Sorting Puzzle",
+        "category": "Puzzle",
+        "image": "https://img.gamemonetize.com/pvktpsvwhq58bo3pe7uqgxalqnwu67pr/512x340.jpg",
+        "url": "https://html5.gamemonetize.co/pvktpsvwhq58bo3pe7uqgxalqnwu67pr/"
+    },
+    {
+        "name": "Short Ride",
+        "category": "Adventure",
+        "image": "https://img.gamemonetize.com/2p9kpyev7iisztb9q8cwfyu9h4f9hlv1/512x384.jpg",
+        "url": "https://html5.gamemonetize.co/2p9kpyev7iisztb9q8cwfyu9h4f9hlv1/"
+    },
+    {
+        "name": "IRON SMASH",
+        "category": "Arcade",
+        "image": "https://img.gamemonetize.com/wf42ep9hw8py43cv4h4vgorep74ylimk/512x384.jpg",
+        "url": "https://html5.gamemonetize.co/wf42ep9hw8py43cv4h4vgorep74ylimk/"
+    },
+    {
+        "name": "Jump Masters",
+        "category": "Arcade",
+        "image": "https://img.gamemonetize.com/oc96sbhgq8x99xtl3ghoqk3nf85i72bs/512x384.jpg",
+        "url": "https://html5.gamemonetize.co/oc96sbhgq8x99xtl3ghoqk3nf85i72bs/",
+        "isNew": true
+    },
+    {
+        "name": "MOMENTUM",
+        "category": "Racing",
+        "image": "https://img.gamemonetize.com/71kap8r3c2kj9n5rl4xt89khiwf2kowt/512x384.jpg",
+        "url": "https://html5.gamemonetize.co/71kap8r3c2kj9n5rl4xt89khiwf2kowt/",
+        "isFeatured": true
+    },
+    {
+        "name": "Mountain Run",
+        "category": "Racing",
+        "image": "https://img.gamemonetize.com/releabfjx2tfx6owemnc1nb9cshzkjwg/512x384.jpg",
+        "url": "https://html5.gamemonetize.co/releabfjx2tfx6owemnc1nb9cshzkjwg/"
+    },
+    {
+        "name": "Stack Tower Pro",
+        "category": "Arcade",
+        "image": "https://img.gamemonetize.com/4kgh5yxxwvzmaixwo6kfzp4ocbu4e99b/512x384.jpg",
+        "url": "https://html5.gamemonetize.co/4kgh5yxxwvzmaixwo6kfzp4ocbu4e99b/"
+    },
+    {
+        "name": "Candy Rain",
+        "category": "Arcade",
+        "image": "https://img.gamemonetize.com/yx65q8unmpfskfwrqx7m7blv4de2s8jh/512x384.jpg",
+        "url": "https://html5.gamemonetize.co/yx65q8unmpfskfwrqx7m7blv4de2s8jh/",
+        "isNew": true
     }
 ];
 
