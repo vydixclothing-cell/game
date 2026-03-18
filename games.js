6e1b59cd851f11391990e82b34fe8a0969d2da97
vydixclothing-cell <vydixclothing@gmail.com>
2026-03-18 22:05:20 +0530

    Game removed: IRON SMASH

--- games.js
+++ games.js
@@ -10,12 +10,6 @@ const games = [
         "category": "Adventure",
         "image": "https://img.gamemonetize.com/2p9kpyev7iisztb9q8cwfyu9h4f9hlv1/512x384.jpg",
         "url": "https://html5.gamemonetize.co/2p9kpyev7iisztb9q8cwfyu9h4f9hlv1/"
-    },
-    {
-        "name": "IRON SMASH",
-        "category": "Arcade",
-        "image": "https://img.gamemonetize.com/wf42ep9hw8py43cv4h4vgorep74ylimk/512x384.jpg",
-        "url": "https://html5.gamemonetize.co/wf42ep9hw8py43cv4h4vgorep74ylimk/"
     }
 ];
 
