de6b4ac1f0f54f049bf59df4477df7d65cf3ae92
vydixclothing-cell <vydixclothing@gmail.com>
2026-03-18 22:23:15 +0530

    Game removed: My Cake Shop: Bake & Serve

--- games.js
+++ games.js
@@ -1,10 +1,3 @@
-const games = [
-    {
-        "name": "My Cake Shop: Bake & Serve",
-        "category": "Adventure",
-        "image": "https://img.gamedistribution.com/bd478cd5791e4568a2352f5a03d35ed1-512x512.jpg",
-        "url": "https://html5.gamedistribution.com/bd478cd5791e4568a2352f5a03d35ed1/"
-    }
-];
+const games = [];
 
 export default games;
