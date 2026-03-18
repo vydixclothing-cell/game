476527a582e9a71a7bdf19a9503046d80bd53188
vydixclothing-cell <vydixclothing@gmail.com>
2026-03-18 22:49:07 +0530

    Game added: Ant-Pil

--- games.js
+++ games.js
@@ -1,3 +1,10 @@
-const games = [];
+const games = [
+    {
+        "name": "Ant-Pil",
+        "category": "Arcade",
+        "image": "https://raw.githubusercontent.com/vydixclothing-cell/game/main/games/images/ant-pil.jpg",
+        "url": "https://vydixclothing-cell.github.io/game/games/ant-pil/index.html"
+    }
+];
 
 export default games;
