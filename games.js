8c54218102072f14f696fd72d365ab383e2a4a53
vydixclothing-cell <vydixclothing@gmail.com>
2026-03-18 22:26:29 +0530

    Update game URL for Stack Tower Pro

--- games.js
+++ games.js
@@ -3,7 +3,7 @@ const games = [
         "name": "Stack Tower Pro",
         "category": "Arcade",
         "image": "https://img.gamedistribution.com/732542dbfb7f4804b95a8ece1e56545a-512x384.jpg",
-        "url": "https://gamedistribution.com/games/stack-tower-pro/"
+        "url": "https://html5.gamedistribution.com/732542dbfb7f4804b95a8ece1e56545a/"
     }
 ];
 
