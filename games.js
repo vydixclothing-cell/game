ff1c4f72652843120b563f23759f5038f5d4e4c5
vydixclothing-cell <vydixclothing@gmail.com>
2026-03-19 13:42:44 +0530

    Game added: split

--- games.js
+++ games.js
@@ -6,6 +6,14 @@ const games = [
         "controls": "- Aim & Power: Click/tap and drag your marble.\n- Shoot: Release to fire.\n- Pause: Click the || button.",
         "image": "https://raw.githubusercontent.com/vydixclothing-cell/game/main/games/images/antpil.jpg",
         "url": "https://vydixclothing-cell.github.io/game/games/antpil/index.html"
+    },
+    {
+        "name": "split",
+        "category": "Arcade",
+        "description": "SPLIT is an underwater survival game where you control a magical bubble rising through the deep ocean. Your goal is simple — survive as long as possible while dodging waves of colorful fish attacking from all directions. The longer you last, the higher your score climbs and the more intense the challenge gets.",
+        "controls": "On desktop:\nUse the Left and Right arrow keys or A and D to drift sideways\nHold Spacebar to split your bubble.\n\nOn mobile: \nTap and hold anywhere on screen to split. \nSwipe or tilt your touch to navigate. \nPress or tap once to start the game and again after dying to resurface.",
+        "image": "https://raw.githubusercontent.com/vydixclothing-cell/game/main/games/images/split.jpg",
+        "url": "https://vydixclothing-cell.github.io/game/games/split/index.html"
     }
 ];
 
