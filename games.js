1bcfcff34f94c8d8b821a89ebba87dcc6fa76036
vydixclothing-cell <vydixclothing@gmail.com>
2026-03-19 12:02:13 +0530

    Game removed: Water Ball Sorting Puzzle

--- games.js
+++ games.js
@@ -4,12 +4,6 @@ const games = [
         "category": "Puzzle",
         "image": "https://img.gamemonetize.com/s48fqhgjkr3e0ty4ylhk98wlghp0jxr0/512x384.jpg",
         "url": "https://html5.gamemonetize.co/s48fqhgjkr3e0ty4ylhk98wlghp0jxr0/"
-    },
-    {
-        "name": "Water Ball Sorting Puzzle",
-        "category": "Puzzle",
-        "image": "https://img.gamemonetize.com/pvktpsvwhq58bo3pe7uqgxalqnwu67pr/512x340.jpg",
-        "url": "https://html5.gamemonetize.co/pvktpsvwhq58bo3pe7uqgxalqnwu67pr/"
     }
 ];
 
