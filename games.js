85d5167deb2079bea6ac1e9077b2dfa5ebacdb69
vydixclothing-cell <vydixclothing@gmail.com>
2026-03-19 12:45:54 +0530

    Game removed: Short Ride

--- games.js
+++ games.js
@@ -4,12 +4,6 @@ const games = [
         "category": "Puzzle",
         "image": "https://img.gamemonetize.com/s48fqhgjkr3e0ty4ylhk98wlghp0jxr0/512x384.jpg",
         "url": "https://html5.gamemonetize.co/s48fqhgjkr3e0ty4ylhk98wlghp0jxr0/"
-    },
-    {
-        "name": "Short Ride",
-        "category": "Adventure",
-        "image": "https://img.gamemonetize.com/2p9kpyev7iisztb9q8cwfyu9h4f9hlv1/512x384.jpg",
-        "url": "https://html5.gamemonetize.co/2p9kpyev7iisztb9q8cwfyu9h4f9hlv1/"
     }
 ];
 
