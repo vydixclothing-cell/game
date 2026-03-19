c49dc608f691c892853f9c8255e97d8d95bb434b
vydixclothing-cell <vydixclothing@gmail.com>
2026-03-19 12:45:56 +0530

    Game removed: Turn on Bulb

--- games.js
+++ games.js
@@ -1,10 +1,3 @@
-const games = [
-    {
-        "name": "Turn on Bulb",
-        "category": "Puzzle",
-        "image": "https://img.gamemonetize.com/s48fqhgjkr3e0ty4ylhk98wlghp0jxr0/512x384.jpg",
-        "url": "https://html5.gamemonetize.co/s48fqhgjkr3e0ty4ylhk98wlghp0jxr0/"
-    }
-];
+const games = [];
 
 export default games;
